bbfd800127cb11fb44a4b9cc44618ef198c139ce
Patrick Barry <cromlyngames@gmail.com>
2016-12-22 22:33:17 +0000

    Update dice.js

--- dice.js
+++ dice.js
@@ -334,7 +334,13 @@ var Dice = function (command, rng) {
             var roll = new Roll(0, sides.length - 1);
             roll.dice = _.range(count).map(() => self.roll(roll.min, roll.max));
             roll.value = roll.dice.reduce((x, y) => x + y);
-            self.comment = sides[roll] + " " + self.comment
+            self.comment = sides[roll.value] + " " + self.comment
+            return roll;
+            
+             var roll = new Roll(1, sides);
+            roll.dice = _.range(count).map(() => self.roll(roll.min, roll.max));
+            roll.value = roll.dice.reduce((x, y) => x + y);
+
             return roll;
         },           
         
